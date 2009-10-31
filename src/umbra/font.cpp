dcb0666ed72d5f7b3a869422cf4e2d98212ff2bf
mingos <unknown>
2009-10-31 20:54:10 +0000

    added the missing font class

---
+++ src/umbra/font.cpp
@@ -0,0 +1,71 @@
+/*
+* Umbra
+* Copyright (c) 2009 Dominik Marczuk, Jice
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * The names of Dominik Marczuk or Jice may not be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY DOMINIK MARCZUK & JICE ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL DOMINIK MARCZUK OR JICE BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "umbra.hpp"
+#include <string>
+
+UmbraFont::UmbraFont (void) {
+    std::string _filename;
+    _filename = "NULL";
+    _rows = 0;
+    _columns = 0;
+    _flags = 0;
+}
+
+void UmbraFont::initialise (int r, int c, const char * fn, int f) {
+    _rows = r;
+    _columns = c;
+    _flags = f;
+    _filename = fn;
+}
+
+void UmbraFont::initialise (int s, const char * fn, int f) {
+    TCODImage * img = new TCODImage(fn);
+    int w, h;
+    img->getSize(&w, &h);
+    _rows = w/s;
+    _columns = h/s;
+    _flags = f;
+    _filename = fn;
+    delete img;
+}
+
+const char * UmbraFont::filename (void) {
+    return _filename.c_str();
+}
+
+int UmbraFont::rows (void) {
+    return _rows;
+}
+
+int UmbraFont::columns (void) {
+    return _columns;
+}
+
+int UmbraFont::flags (void) {
+    return _flags;
+}
