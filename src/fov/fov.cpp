81484cfc2c1dd6a6d5ccd15e3c27b3047648cc8d
mingos <unknown>
2009-11-24 20:29:46 +0000

    added the new files... for real this time...

---
+++ src/fov/fov.cpp
@@ -0,0 +1,46 @@
+/*
+* Umbra
+* Copyright (c) 2009 Dominik Marczuk, Jice
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * The names of Dominik Marczuk or Jice may not be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY DOMINIK MARCZUK & JICE ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL DOMINIK MARCZUK OR JICE BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "../demo/main.hpp"
+#include "fov.hpp"
+
+TCODRandom * FOV::random;
+uint32 FOV::seed;
+
+FOV::FOV (void) {
+    if (!random) random = TCODRandom::getInstance();
+    accumulatedTime = 0;
+}
+
+void FOV::initialise (void) {
+    if (!seed) seed = random->getInt(0,0xFFFFFFFF);
+}
+
+void FOV::activate (void) {
+    delete random;
+    random = new TCODRandom(seed);
+}
