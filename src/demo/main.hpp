d80bfba1d5fe6df7ba82078c2d55dcb5c8ab171c
Kyle Benesch <4b796c65+github@gmail.com>
2022-09-13 18:56:18 -0700

    Add CMake/Vcpkg build files.

--- src/demo/main.hpp
+++ src/demo/main.hpp
@@ -25,7 +25,7 @@
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include "../umbra/umbra.hpp"
+#include <umbra/umbra.hpp>
 #include "matrix.hpp"
 #include "rabbit.hpp"
 #include "demo.hpp"
